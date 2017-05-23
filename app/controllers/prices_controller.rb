5731604615b4464c4c33c21438e2470956583e1a
Lynda Guillu <lynda_guillu@att.net>
2017-05-22 20:51:47 -0500

    Pull Needed Data

--- app/controllers/prices_controller.rb
+++ app/controllers/prices_controller.rb
@@ -1,8 +1,11 @@
 class PricesController < ApplicationController
   def index
     @data = Unirest.get("http://api.eia.gov/series/?api_key=bea6202b7a787c0b4d1daf15108ae4f7&series_id=PET.EMM_EPMM_PTE_YORD_DPG.W").body
-    @price = @data["series"][0]["data"]
-    render "index.html.erb"
+    @series_data = @data["series"][0]["data"]
+    @geography = @data["series"][0]["geography"]
+    @name = @data["series"][0]["name"]
+    @units = @data["series"][0]["units"]
+   render "index.html.erb"
   end
 
 end
