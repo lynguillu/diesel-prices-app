a335374cd59408faabb9f7bade2900a496673461
Lynda Guillu <lynda_guillu@att.net>
2017-05-22 22:52:23 -0500

    Update Midwest Gas Prices

--- app/controllers/prices_controller.rb
+++ app/controllers/prices_controller.rb
@@ -1,6 +1,6 @@
 class PricesController < ApplicationController
   def index
-    @data = Unirest.get("http://api.eia.gov/series/?api_key=bea6202b7a787c0b4d1daf15108ae4f7&series_id=PET.EMM_EPMM_PTE_YORD_DPG.W").body
+    @data = Unirest.get("http://api.eia.gov/series/?api_key=bea6202b7a787c0b4d1daf15108ae4f7&series_id=PET.EMD_EPD2D_PTE_R20_DPG.W").body
     @series_data = @data["series"][0]["data"]
     @geography = @data["series"][0]["geography"]
     @name = @data["series"][0]["name"]
